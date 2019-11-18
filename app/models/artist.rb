1dae5f6eb06fb94800a250673f7c285862df0bca
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-18 21:45:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,8 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
-  def get_first_song
-    self.songs.first
-  end
+  
+
   
   def get_genre_of_first_song
    self.songs.
